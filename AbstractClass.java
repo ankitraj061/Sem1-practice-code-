4426469fa8e65cc7c4feda65151a066197eec0ef
ankitraj061 <ankitraj07032002@gmail.com>
2023-12-06 18:36:15 +0530

    i made a change

--- AbstractClass.java
+++ AbstractClass.java
@@ -1,9 +1,9 @@
 abstract class AbstractClass {
     public static void main(String[] args) {
         System.out.println("hello");
-        int arr[]=new int[6];
+        int arr[] = new int[6];
         System.out.println(arr[7]);
-        
+        System.out.println();
+
     }
 }
-    
